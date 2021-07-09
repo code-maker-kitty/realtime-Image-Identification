32857b2b129f4f24889aefde5494cfaf33d33989
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-07-08 17:41:23 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-https://teachablemachine.withgoogle.com/models/g6otHPQcT/
+
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/g6otHPQcT/',modelLoaded);
+}
+function modelLoaded() {
+    console.log('model Loaded!');
 }
 function draw(){
-    
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotresult);
+}
+function gotresult(error, result){
+    if (error){
+        console.error(error);
+    }else{
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
